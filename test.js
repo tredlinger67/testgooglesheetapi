ddc06a918cb214a379d7514cad8b811cd41d040a
tredlinger67 <102240895+tredlinger67@users.noreply.github.com>
2022-03-23 10:05:28 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,36 @@
+function testGS(){
+
+    const url = "https://script.google.com/macros/s/AKfycbyXh2Jens7DfOugO7lGJqr1uLdoAI25PRThOJ6bs5-QLJ9XqiudjZXa9RQuAIlTePxH/exec";
+
+
+    fetch(url)
+        .then(d => d.json())
+        .then(d => {
+            document.getElementById("app").textContent = d[0].status;
+        });
+
+}
+
+
+function addGS(){
+
+    const url = "https://script.google.com/macros/s/AKfycbyXh2Jens7DfOugO7lGJqr1uLdoAI25PRThOJ6bs5-QLJ9XqiudjZXa9RQuAIlTePxH/exec";
+
+
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({first:"Jake",phone:"312-000-1212", last:"Newperson"}) // body data type must match "Content-Type" header
+      });
+
+}
+
+document.getElementById("btn2").addEventListener("click",addGS);
+
+document.getElementById("btn").addEventListener("click",testGS);
